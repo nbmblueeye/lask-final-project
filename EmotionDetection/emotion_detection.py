dac915db7091ae1554ec4ae40f8d0e6022824ea4
nbmblueeye <nbmblueeye@gmail.com>
2024-06-15 09:47:09 -0400

    Completed task 4

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -11,12 +12,25 @@ def emotion_detector(text_to_analyze):
 
     # Add text to text_to_analyze
     ## Check if emotion_predictions, the list is not empty
-    if emotion_predictions && 'emotionMentions' in emotion_predictions[0]:
+    if emotion_predictions and 'emotionMentions' in emotion_predictions[0]:
         emotion_mentions = emotion_predictions[0]['emotionMentions']
-        
+
         ## Check if emotion_mentions, the list is not empty
         if  emotion_mentions:
             text = emotion_mentions[0]['span']['text']  # Access the 'text' field
-            return text
 
-    return ({'message': 'text is not found'})
+            ## Convert the response text into a dictionary using the json library functions.
+            text_dictionary = json.dumps({'text': text})
+            
+            ## Extract the required set of emotions, including anger, disgust, fear, joy and sadness
+            emotions = emotion_mentions[0]['emotion']
+
+            ## Find the dominant emotion based on the highest score
+            dominant_emotion = max(emotions, key=emotions.get)
+            
+            ## Add dominant_emotion to the emotion dictionary
+            emotions['dominant_emotion'] = dominant_emotion
+            return emotions
+
+    return ({'message': 'Error is occured'})
+    
