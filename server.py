976d327dc3197f68396699e570991c2647628baa
nbmblueeye <nbmblueeye@gmail.com>
2024-06-15 11:38:24 -0400

    completed application useign flask

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 from EmotionDetection import emotion_detector
+import os
 
-app = Flask(__name__)
+# Define the template directory
+template_dir = os.path.abspath('final_project/oaqjp-final-project-emb-ai/templates')
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route("/emotionDetector", methods=['GET'])
 def detect_emotion():
@@ -23,6 +26,14 @@ def detect_emotion():
 
         responseText = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}." 
 
-        return render_template('index.html', responseText=responseText), 200
+        return render_template('index.html', responseText="responseText"), 200
     else:
         return render_template('index.html', responseText="No emotions detected."), 400
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
